5e332ebc81073bdb54c0e9008adb0de885ad2278
ahmetakdeve <55084294+ahmetakdeve@users.noreply.github.com>
2019-10-06 18:21:10 +0200

    Travis added

---
+++ swelection/R/lab5_17_Package.R
@@ -0,0 +1,12 @@
+#' Sweden election 2014
+#'
+#' This package is used to get the 2014 election results in Sweden for the nine biggest parties.The package contains two functions which are "votes_spec" and "votes_sum".
+#' 
+#' @docType package
+#'
+#' @author Ahmet Akdeve \email{ahmak554@student.liu.se}
+#' @author Zhixuan Duan \email{darinstu999@gmail.com}
+#'
+#' @name swelection
+#' 
+NULL
